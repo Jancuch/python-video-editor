a0fc75b5a9c0938c8bfab42d0f04ec15d3be4851
Jancuch <kupaguj@gmail.com>
2020-07-25 22:10:14 +0200

    xd

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ clip = VideoFileClip("./data/videos/pyro.mp4").subclip(0 , 23)
 audio = AudioFileClip("./data/audio/reposted.mp3")
 clip1 = VideoFileClip("./data/videos/pyro.mp4").subclip(10 ,20)
 
-final = concatenate_videoclips([clip , clip1 , clip1, clip1, clip1, clip1])
-final = crop(final , x1=175   ,y1=150)
-final1 = final.set_audio(audio)
+logo = ImageClip("./data/images/ask.png")
 
-final1.write_videofile("./output/e-pyro.mp4")
-final2 = VideoFileClip("./output/e-pyro.mp4").subclip(1 , 40)
-final2.write_videofile("./output/e-pyro1.mp4")
+logo = logo.resize((800,100)) # if you need to resize...
+#logo.margin(right=8, top=8, opacity=0) # (optional) logo-border padding
+logo = logo.set_position(("center","top"))
+
+
+final = concatenate_videoclips([clip , clip1 , clip1, clip1, clip1, clip1])
+
+(w, h) = clip.size
+print(w , h)
+final = crop(final, width=800, height=800, x_center=800, y_center=800)
+final = final.set_audio(audio)
+final = CompositeVideoClip([final, logo.set_duration(final.duration)])
+final = final.subclip(0 , 40)
+final.write_videofile("./output/e-pyro.mp4")
