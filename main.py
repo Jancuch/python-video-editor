8b41c5477de109166d2f475b2c1929d4591eab8a
Jancuch <kupaguj@gmail.com>
2020-07-28 21:47:48 +0200

    dasdad

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class memegen():
         
         def videocut(video , loopinfo , loopcount , audio):
             audio1 = AudioFileClip("./data/audio/lol.mp3")
-            
+            audio1 = audio1.volumex(0)
             clip = VideoFileClip(video).subclip(0, loopinfo[2])
             clip1 = VideoFileClip(video).subclip(loopinfo[0] , loopinfo[1])
             
@@ -58,70 +58,43 @@ class memegen():
 
 
         def render(video):
-            video = video.write_videofile('./output/outpu1.mp4', codec='libx264', audio=True, audio_codec='aac')
+            video = video.write_videofile('./output/output2.mp4', codec='libx264', audio=True, audio_codec='aac')
 
         ren = videocut(video , loopinfo , loopcount, audio)
         ren = addImage(ren , "./data/images/ask.png")
         render(ren)
-        #if image == None:
-#
-        #    clip = VideoFileClip(video).subclip(0 , loopinfo[2])
-        #    clip1 = VideoFileClip(video).subclip(loopinfo[0] , loopinfo[1])
-        #    audio = AudioFileClip(audio)
-        #   
-        #    clip1 = loop(clip1 , loopcount)
-        #    final = concatenate_videoclips([clip , clip1])
-        #    (w, h) = clip.size
-        #    final = crop(final, width=w/2, height=800, x_center=w/2, y_center=h/2)
-        #    final = final.set_audio(audio)
-        #    countlooptime = loopinfo[1] - loopinfo[0] 
-        #    countlooptime = countlooptime * loopcount + loopinfo[2]
-        #    final = final.subclip(0 , countlooptime)
-        #    final.write_videofile("./output/output.mp4")
-#
-        #if loopinfo:
-        #    clip = VideoFileClip(video).subclip(0 , loopinfo[2])
-        #    clip1 = VideoFileClip(video).subclip(loopinfo[0] , loopinfo[1])
-        #    audio = AudioFileClip(audio)
-        #    image = ImageClip(image)
-        #    image = image.set_position(("center","top"))
-        #    clip1 = loop(clip1 , loopcount)
-        #    final = concatenate_videoclips([clip , clip1])
-        #    (w, h) = clip.size
-        #    
-        #    final = crop(final, width=w/2, height=800, x_center=w/2, y_center=h/2)
-        #    final = final.set_audio(audio)
-        #    final = CompositeVideoClip([final, image.set_duration(final.duration)])
-        #    countlooptime = loopinfo[1] - loopinfo[0] 
-        #    countlooptime = countlooptime * loopcount + loopinfo[2]
-        #    final = final.subclip(0 , countlooptime)
-        #    final.write_videofile("./output/output.mp4")
-        #
-        #else:
-        #    clip = VideoFileClip(video)
-        #    image = ImageClip(image)
-        #    image = image.set_position(("center","top"))
-        #    (w, h) = clip.size
-        #    final = VideoFileClip.subclip(0 , clip.duration)
-        #    final = crop(final, width=800, height=800, x_center=w/2, y_center=h/2 -100)
-        #    final = CompositeVideoClip([final, image.set_duration(final.duration)])
-        #    final.write_videofile("./output/output.mp4")
 
-    #def imagegen(self, path , txt , fsize , orientation):
-    #    image = Image.open(path)
-    #    draw = pil.ImageDraw(image)
-    #    size1 = size
-    #    font = ImageFont.truetype(r'./data/arial.ttf' , size=size1)
-    #    txt1 = txt
-    #    draw.text((5,5),font=font , align=orientation , text=txt1)
-    #    image.show()
-    #    
-    #    image = image.save('./data/images/ask1.png')
-    #    return path
+    def imagegen(self, txt , fsize):
+        new_txt = []
+        letter_count = 0 
+        for letter in txt:
+            new_txt.append(letter)
+            letter_count = letter_count + 1 
+            if letter_count == 19:
+                new_txt.append('\n')
+                letter_count = 0
+                
+                
+        print(new_txt)
+        stringconv = ''.join(map(str , new_txt))
+        print(stringconv)
+        image = Image.open('./data/images/ask.png')
+        font = ImageFont.truetype(r'./data/arial.ttf' , fsize)
+        
+        draw = pil.Draw(image)
+        draw.text((30 , 0 ) , stringconv , fill=(255,255,255) , align='center' , font=font )
+        print(image.size[0])
+        #draw.text(txt)
+        
+        image.show()
+        
+        image = image.save('./data/images/ask1.png')
+        return image
+        
 
 meme = memegen()
-#meme.imagegen('./data/images/ask.png' , '5', 65 ,  "center")
-meme.videogen("./data/videos/pyro.mp4" , "./data/audio/5g.mp3",None , [10 , 20 ,23] , 5)
+img =meme.imagegen( '0123456789012345678901234567890sssssssssssssssssssssssssssssssssssssssssssss' , 70)
+meme.videogen("./data/videos/pyro.mp4" ,audio=None,  image="./data/images/plz1" , loopinfo=[10 , 20 ,23] , loopcount=10)
 
 
 
